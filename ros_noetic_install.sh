238917943709d20b5d0b23db9385f39d9b80ace3
SEUZTh <50619570+SEUZTh@users.noreply.github.com>
2021-08-29 18:40:00 +0800

    Update ros_noetic_install.sh

--- ros_noetic_install.sh
+++ ros_noetic_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-## Author: 宇宙爆肝锦标赛冠军
-## Time: 2021.07.25
+## Author: ZTh
 ## ROS Gazebo: http://wiki.ros.org/kinetic/Installation/Ubuntu
 
 mypassword="$1"
 
 ## 1.2 Setup your sources.list
 echo 'mypassword' |sudo -S sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+# sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 ## 1.3 Set up your keys
 sudo apt -y install curl
